0f05ec0ed2f3e28740d8ca62dfb40507283ca232
Joaco <montesano1994@gmail.com>
2023-06-14 20:41:35 -0300

    upload commit

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,6 +1,24 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
-const ProductCard = ({ product }) => {
+interface Product {
+    name: string;
+    description1: string;
+    description2: string;
+    price: number;
+    image: string;
+  }
+  
+  interface ProductCardProps {
+    product: Product;
+  }
+  
+
+  const ProductCard = ({ product }: ProductCardProps) => {
+
+    const handleButtonClick = () => {
+        window.location.href = 'https://wa.link/8gwob4';
+      };
   return (
     <div className='productCard-Container'>
         <div className='productCard-Container__image'>
@@ -11,7 +29,9 @@ const ProductCard = ({ product }) => {
       <h4>{product.description2}</h4>
       <div style={{display: 'flex', justifyContent: 'space-around', paddingRight:'20px',alignItems:'center', marginTop:'20px'}}>
       <h3>{product.price}</h3>
-      <button>Preguntar Stock</button>
+      <Link href='https://wa.link/8gwob4' passHref>
+          <button>Preguntar Stock</button>
+        </Link>
       </div>
     </div>
   );
