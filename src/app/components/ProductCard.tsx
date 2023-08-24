f7d4160a5a2351378a3a9e4ff87a5657e8897372
Joaco Montesano <montesano1994@gmail.com>
2023-08-24 16:40:41 -0300

    change conditional

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,41 +1,54 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 interface Product {
-    name: string;
-    description1: string;
-    description2: string;
-    price: string;
-    image: string;
-  }
-  
-  interface ProductCardProps {
-    product: Product;
-  }
-  
+  name: string;
+  description1: string;
+  description2: string;
+  price: string;
+  image: string;
+}
 
-  const ProductCard = ({ product }: ProductCardProps) => {
+interface ProductCardProps {
+  product: Product;
+}
 
-    const handleButtonClick = () => {
-        window.location.href = 'https://wa.link/8gwob4';
-      };
+const ProductCard = ({ product }: ProductCardProps) => {
+  const handleButtonClick = () => {
+    window.location.href = "https://wa.link/8gwob4";
+  };
   return (
-    <div className='productCard-Container'>
-        <div className='productCard-Container__image'>
-            <Image src={`${product.image}`}alt={product.name} width={300} height={300} />
-        </div>
-      <h1>{product.name}</h1>
-      <h4>{product.description1}</h4>
-      <h4>{product.description2}</h4>
-      <div style={{display: 'flex', justifyContent: 'space-around', paddingRight:'20px',alignItems:'center', marginTop:'20px'}}>
-      <h3>{product.price}</h3>
-      <Link href='https://wa.link/8gwob4' passHref>
+    <div className="productCard-Container">
+      <div className="productCard-Container__image">
+        <Image
+          src={`${product.image}`}
+          alt={product.name}
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>{product.name && product.name}</h1>
+
+      {product.description1 && <h4>{product.description1}</h4>}
+
+      {product.description2 && <h4>{product.description2}</h4>}
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          paddingRight: "20px",
+          alignItems: "center",
+          marginTop: "20px",
+        }}
+      >
+        {product.price && <h3>{product.price}</h3>}
+        <Link href="https://wa.link/8gwob4" passHref>
           <button>Preguntar Stock</button>
         </Link>
       </div>
     </div>
   );
-}
+};
 
-  
-  export default ProductCard;
+export default ProductCard;
